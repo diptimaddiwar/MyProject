074b5082876b7a7bbda5f22142e7efb94d32a22f
Dipti Maddiwar <diptimaddiwar@gmail.com>
2026-09-11 21:35:40 -0400

    adding right file

--- src/test/java/seleniumgitjenkin/SleniumJenkinsFlowTest.java
+++ src/test/java/seleniumgitjenkin/SleniumJenkinsFlowTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SleniumJenkinsFlow {
+public class SleniumJenkinsFlowTest {
 
 	public static void main(String[] args) {
 		
